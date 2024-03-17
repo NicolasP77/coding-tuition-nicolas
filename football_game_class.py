ff5730aa273bb9befabf7cdc4197955d9cd290f6
NicolasP77 <nicolas.pigs@gmail.com>
2024-03-17 19:04:13 +0000

    17/03/24

--- football_game_class.py
+++ football_game_class.py
@@ -2,6 +2,7 @@ import turtle
 turtle.register_shape("footballer.gif")
 turtle.register_shape("football.gif")
 GRAVITY = 0.001
+PLAYER = 0.1
 
 class CustomTurtle(turtle.Turtle):
     def __init__(self, x, y, shape):
@@ -13,9 +14,21 @@ class CustomTurtle(turtle.Turtle):
     #collision method
 
 class Player(CustomTurtle):
-    def __init__(self, x, y):
+    def __init__(self, x, y, window_width):
         super().__init__(x = x, y = y, shape = "footballer.gif")
+        self.window_width = window_width
 
+    def move_right(self):
+        if self.xcor() < self.window_width // 2:
+            self.setx(self.xcor() + 1)
+
+    def move_left(self):
+        if self.xcor() > self.window_width // -2:
+            self.setx(self.xcor() - 1)
+    def on_collision(self, football_velocity, football_direction):
+        football_velocity += PLAYER
+        football_direction = (football_direction + 180) % 360
+        return football_direction, football_velocity
 
 
 class Goalkeeper(CustomTurtle):
@@ -28,10 +41,13 @@ class Goalkeeper(CustomTurtle):
 
 
 class Football(CustomTurtle):
-    def __init__(self, x, y):
+    def __init__(self, x, y,):
         super().__init__(y = y, x = x, shape = "football.gif")
         self.velocity = 0
-    def move_gravity(self):
+        self.direction = 0
+    def move_gravity(self, player):
         self.velocity -= GRAVITY
         self.sety(self.ycor() + self.velocity)
+        if self.distance(player) < 20:
+            player.on_collision(self.velocity, self.direction)
 
